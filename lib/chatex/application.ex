0b1001d482065a84ec4b37ac93d615e85a6a134e
rodrigocitadin <citadinrodrigo@gmail.com>
2025-09-01 02:11:31 -0300

    feat: auto connect nodes when started

--- lib/chatex/application.ex
+++ lib/chatex/application.ex
@@ -8,6 +8,7 @@ defmodule Chatex.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      Chatex.Node
       # Starts a worker by calling: Chatex.Worker.start_link(arg)
       # {Chatex.Worker, arg}
     ]
---
+++ lib/chatex/node.ex
@@ -0,0 +1,32 @@
+defmodule Chatex.Node do
+  use GenServer
+
+  def start_link(_), do: GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+
+  def init(_) do
+    schedule_connect()
+    {:ok, nil}
+  end
+
+  def handle_info(:connect, state) do
+    case :net_adm.names() do
+      {:ok, names} ->
+        nodes =
+          Enum.map(names, fn {name, _port} ->
+            :"#{name}@#{:net_adm.localhost()}"
+          end)
+
+        Enum.each(nodes, fn n ->
+          if n != Node.self(), do: Node.connect(n)
+        end)
+
+      _ ->
+        IO.puts("I couldn't get the list of nodes from epmd")
+    end
+
+    schedule_connect()
+    {:noreply, state}
+  end
+
+  defp schedule_connect(), do: Process.send_after(self(), :connect, 5_000)
+end
